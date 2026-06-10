d9268528c44acf91ae33892206d4fb821219349c
Nadja Krivokapic <nadja.krivokapic04@gmail.com>
2026-06-10 11:46:03 +0200

    Changed some comments

--- src/knr.py
+++ src/knr.py
@@ -26,12 +26,12 @@ from preprocess import y_train_lin as y_train
 #     knr.fit(X_train, y_train)
 #     y_pred = knr.predict(X_val)
 
-#     # koristim rmse za optimizaciju suseda parametra
+#     # koristim rmse za optimizaciju sused parametra
 #     greska[sused] = root_mean_squared_error(y_val, y_pred)
 
 # # Ispis 10 najlošijih suseda (najveća greška)
 # najlosiji_sused = sorted(greska.items(), key=lambda x: x[1], reverse=True)[:10]
-# print("\n10 najlosijih alfa (najveca RMSE greska):")
+# print("\n10 najlosijih suseda (najveca RMSE greska):")
 # for sused, rmse_val in najlosiji_sused:
 #     print(f"  sused = {sused:.6f}  =>  RMSE = {rmse_val:.4f}")
 
