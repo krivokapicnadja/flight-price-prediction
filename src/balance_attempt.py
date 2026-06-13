f050bb199e0d21e83e81a1a42c04a190ce76ec63
Nadja Krivokapic <nadja.krivokapic04@gmail.com>
2026-06-13 15:06:20 +0200

    Added encoder saving function

--- src/balance_attempt.py
+++ src/balance_attempt.py
@@ -1,8 +1,8 @@
-#iz important_features.py je zakljuceno da class ima najveci uticaj na cenu.
-#buduci da bussines ima duplo manje od economy klase, probacemo da ih izbalansiramo
-#i tako probamo istrenirati model
+# iz important_features.py je zakljuceno da class ima najveci uticaj na cenu.
+# buduci da bussines ima duplo manje od economy klase, probacemo da ih izbalansiramo
+# i tako probamo istrenirati model
 
-#probacu sa svim tree modelima jer daju slicne metrike
+# probacu sa svim tree modelima jer daju slicne metrike
 import pandas as pd
 import os
 import joblib
@@ -23,15 +23,12 @@ DATA_PATH = os.path.join(BASE_DIR, "dataset", "raw", "Clean_Dataset.csv")
 dataset = pd.read_csv(DATA_PATH)
 
 # Odvoji Economy i Business - ovo su delovi dataseta po klasama odvojeni, ali citavi, sa svim atributima
-dataset_economy = dataset[dataset['class'] == 'Economy']   # ~66% uzoraka
-dataset_business = dataset[dataset['class'] == 'Business'] # ~33% uzoraka
+dataset_economy = dataset[dataset["class"] == "Economy"]  # ~66% uzoraka
+dataset_business = dataset[dataset["class"] == "Business"]  # ~33% uzoraka
 
 # Oversample Business na broj Economy uzoraka (balansira samo Business)
 dataset_business_upsampled = resample(
-    dataset_business,
-    replace=True,
-    n_samples=len(dataset_economy),
-    random_state=42
+    dataset_business, replace=True, n_samples=len(dataset_economy), random_state=42
 )
 # Spoji ponovo
 dataset_balanced_classes = pd.concat([dataset_economy, dataset_business_upsampled])
@@ -70,7 +67,8 @@ models = {
     "Random Forest": RandomForestRegressor(
         n_estimators=150,  # najbolji_estimator
         min_samples_leaf=2,
-        min_samples_split=20),
+        min_samples_split=20,
+    ),
     "Gradient Boosting": GradientBoostingRegressor(
         n_estimators=700,  # best_params[1]
         learning_rate=0.5,
@@ -78,7 +76,8 @@ models = {
         min_samples_split=2,
         min_samples_leaf=1,
         subsample=0.8,
-        random_state=42)
+        random_state=42,
+    ),
 }
 
 trained_models = {}
@@ -102,7 +101,6 @@ for model_name, regressor in models.items():
             "mae": mean_absolute_error(y_test, y_pred),
             "rmse": root_mean_squared_error(y_test, y_pred),
             "r2 score": r2_score(y_test, y_pred),
-
         }
     )
 results_data = pd.DataFrame(results)
@@ -117,13 +115,17 @@ METRICS_FILE = os.path.join(METRICS_DIR, "balanced_metrics.csv")
 
 results_data.to_csv(METRICS_FILE, index=False)
 
-#nakon prvog pokretanja, vidim da je sa ovako izbalansiranim klasama bolji rezultat nego bez
-#najbolji rmse ima gradient boosting, to uzimam kao merodavnu metriku jer je veca od mae - znaci da taj model najbolje koriguje velike greske tj outliere
-#cuvacu gradient boosting kao najbolji model za deployment
+# nakon prvog pokretanja, vidim da je sa ovako izbalansiranim klasama bolji rezultat nego bez
+# najbolji rmse ima gradient boosting, to uzimam kao merodavnu metriku jer je veca od mae - znaci da taj model najbolje koriguje velike greske tj outliere
+# cuvacu gradient boosting kao najbolji model za deployment
 
 MODELS_DIR = os.path.join(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "models"
 )
 os.makedirs(MODELS_DIR, exist_ok=True)
-joblib.dump(trained_models["Gradient Boosting"], os.path.join(MODELS_DIR, "deploy.joblib"))
-
+joblib.dump(
+    trained_models["Gradient Boosting"], os.path.join(MODELS_DIR, "deploy.joblib")
+)
+joblib.dump(
+    transformer_forest, os.path.join(MODELS_DIR, "encoder.joblib")
+)  # cuvam encoder zbog apija
